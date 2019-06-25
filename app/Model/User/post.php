2f77f3a48b0c1f59d563e03a26487d280cef2862
mdashikulislam <ashik713832@gmail.com>
2019-06-25 11:29:44 +0600

    make many  relationship between post, category and tag

--- app/Model/User/post.php
+++ app/Model/User/post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class post extends Model
 {
-    //
+    public function tags(){
+        return $this->belongsToMany(tag::class,'post_tags');
+    }
+    public function categories(){
+        return $this->belongsToMany(category::class,'category_posts');
+    }
 }
